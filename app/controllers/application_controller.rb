6fb21853a4caf0db331e3133832a4dc264484fe9
Steve Organ <steve@influitive.com>
2013-04-06 14:00:31 -0400

    another styles update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  #def after_sign_in_path_for(resource)
+  #	@url = url_for(:only_path => false)
+
+  #	if @url != leagues_path
+  #		players_path
+  #	else
+	#		leagues_path
+	#	end
+	#end
+
 end
